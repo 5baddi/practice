009ec1fd2385c1749270908e60bb0b76f5eaa4b3
5Baddi <project@baddi.info>
2018-03-14 17:07:32 +0000

    Intro to PDO

--- PDO/intro.php
+++ PDO/intro.php
@@ -7,7 +7,7 @@
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); // Set fetching mode as Object
         $query = $db->query("SELECT * FROM clients"); // Simple query
         while($r = $query->fetch()){ // Simple fetch result
-            echo $r->nom;
+            echo $r->name;
         }
     }catch(PDOException $ex){
         // Handle exception
