1b451a7c10f679451cf57f8bcca7647043fa5acd
5Baddi <project@baddi.info>
2018-03-14 21:57:24 +0000

    Update PDO Intro

--- PDO/intro.php
+++ PDO/intro.php
@@ -2,7 +2,11 @@
     print_r(PDO::getAvailableDrivers()); // Check available PDO drivers
 
     try{
-        $db = new PDO("mysql:host=127.0.0.1;dbname=exam", "root", ""); // Connect to db with mysql driver
+        $dsn = "mysql:host=127.0.0.1;dbname=exam"; // DB DSN source
+        $username = "root"; // DB user
+        $password = ""; // DB pass
+
+        $db = new PDO($dsn, $username, $password); // Connect to db with mysql driver
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); // Set fetching mode as Object
         $query = $db->query("SELECT * FROM clients"); // Simple query
